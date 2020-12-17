69077348f33076992d1f48ac7ddabe403be7a858
SmallOwl <maks.sony.ru@gmail.com>
2020-12-17 22:33:00 +0300

    Add Лабораторная4

--- Лабораторные/Лабораторная4.java
+++ Лабораторные/Лабораторная4.java
@@ -3,156 +3,207 @@ package Лабораторные;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import Практики.hash;
 
 public class Лабораторная4 {
 
+    //Факторы протокола
+    static BigInteger N;
+    static BigInteger g;
+    static BigInteger k;
+    //Данные клиента
+    static String clientSalt;
+    static BigInteger clientX;
+    static String clientPassword;
+    static BigInteger clientV;
+    static BigInteger clientA;
+    static BigInteger clientB;
+    static BigInteger clientU;
+    static BigInteger clientS;
+    static BigInteger clientSmallA;
+    static BigInteger clientK;
+    static BigInteger clientR;
+    //Данные сервера
+    static String serverSalt;
+    static BigInteger serverV;
+    static String serverI;
+    static BigInteger serverA;
+    static BigInteger serverB;
+    static BigInteger serverU;
+    static BigInteger serverS;
+    static BigInteger serverK;
+    static BigInteger serverR;
+
     public static void main(String args[]) throws NoSuchAlgorithmException {
-        //Факторы протокола
-        BigInteger q = getQ(100, 2000);
-        BigInteger n = getN(q);
-        BigInteger g = getG(n);
-        BigInteger k = getK();
-        System.out.println("---------------------------------------------------");
-        System.out.println("Факторы протокола");
-        System.out.println("q:\t" + q);
-        System.out.println("n:\t" + n);
-        System.out.println("g:\t" + g);
-        System.out.println("k:\t" + k);
-        System.out.println("---------------------------------------------------");
-        //Шаг 1 - Регистрация
-        System.out.println("Шаг 1 - Регистрация");
-        //Генерация на клиенте
-        String clientS = getSalt(300);
-        String clientP = "myPassword";
-        byte[] clientX = hash(clientS, clientP);
-        BigInteger clientV = getV(g, clientX, n);
-        System.out.println("Генерация на клиенте");
-        System.out.println("clientS:\t" + clientS);
-        System.out.println("clientP:\t" + clientP);
-        System.out.println("clientX:\t");
-        for (byte b : clientX) {
-            System.out.print(b + "\s");
-        }
-        System.out.println();
-        System.out.println("clientV:\t" + clientV);
-        //Сохранение на сервере
-        String serverI = "myUserName";
-        String serverS = clientS;
-        BigInteger serverV = clientV;
-        System.out.println("Сохранение на сервере");
-        System.out.println("serverI:\t" + serverI);
-        System.out.println("serverS:\t" + serverS);
-        System.out.println("serverV:\t" + serverV);
-        System.out.println("---------------------------------------------------");
-        //Шаг 2 - Аутенфикация
-        //Фаза 1
-        System.out.println("Шаг 2 - Аутенфикация");
-        System.out.println("Фаза 1");
-        BigInteger clientSmallA = BigInteger.valueOf(new Random().nextInt());
-        BigInteger clientA = getA(g, clientSmallA, n);
-        System.out.println("clientSmallA:\t" + clientSmallA);
-        System.out.println("clientA:\t" + clientA);
-        if(clientA.compareTo(BigInteger.ZERO) == 0){
-            System.out.println("A == 0");
+        protocolFactorsInit(5);
+        //Этап - 1 - Регистрация
+        clientRegistration();
+        serverRegistrationHandler(clientSalt, clientV);
+        //Этап - 2 - Аутенфикация
+        //Фаза - 1
+        clientSendIandA();
+        serverHandlerIandA(serverI, clientA, serverV);
+        clientBuildKey(serverB);
+        //Фаза - 2
+        getClientMandR();
+        getServerMandR();
+    }
+
+    //Этап - 1 - Регистрация
+    //Клиентская часть
+    private static void clientRegistration() throws NoSuchAlgorithmException {
+        clientSalt = getSalt(300);
+        clientPassword = "myPass";
+        clientX = new BigInteger(hash(clientSalt, clientPassword));
+        clientV = g.modPow(clientX, N);
+    }
+
+    //Этап - 1 - Регистрация
+    //Серверная часть
+    private static void serverRegistrationHandler(String clientS, BigInteger clientV){
+        serverSalt = clientSalt;
+        serverI = "myUserName";
+        serverV = clientV;
+    }
+
+    //Этап - 2 - Аутенфикация
+    //Фаза - 1
+    //Клиентская часть
+    private static void clientSendIandA(){
+        //Тут происходит отправка I
+        clientSmallA = simpleGenerate.getRandomBigInteger(300, false);
+        clientA = g.modPow(clientSmallA, N);
+    }
+
+    private static void clientBuildKey(BigInteger serverB) throws NoSuchAlgorithmException {
+        clientU = new BigInteger(hash(clientA.toString(), serverB.toString()));
+        if(clientU.compareTo(BigInteger.ZERO) == 0){
             System.exit(-1);
         }
-        BigInteger serverSmallB = BigInteger.valueOf(new Random().nextInt());
-        BigInteger serverB = getB(k, serverV, g, serverSmallB, n);
-        System.out.println("serverSmallB:\t" + serverSmallB);
-        System.out.println("serverB:\t" + serverB);
-        clientS = serverS;
-        BigInteger clientB = serverB;
-        if(serverB.compareTo(BigInteger.ZERO) == 0){
-            System.out.println("B == 0");
+        clientX = new BigInteger(hash(serverSalt.toString(), clientPassword.toString()));
+        clientS = serverB.subtract(k.multiply(g.modPow(clientX, N))).modPow(clientSmallA.add(clientU.multiply(clientX)), N);
+        clientK = new BigInteger(hash(clientS.toString()));
+    }
+
+    //Этап - 2 - Аутенфикация
+    //Фаза - 1
+    //Серверная часть
+    private static void serverHandlerIandA(String I, BigInteger A, BigInteger v) throws NoSuchAlgorithmException {
+        if(A.compareTo(BigInteger.ZERO) == 0){
             System.exit(-1);
         }
-        byte[] hab = hash(clientA.toString(), serverB.toString());
-        if(hab.length == 1 && hab[0] == 0){
-            System.out.println("H(A,B) == 0");
+        serverA = A;
+        BigInteger b = simpleGenerate.getRandomBigInteger(300, false);
+        serverB = k.multiply(v).add(g.modPow(b, N)).mod(N);
+        //I(username) уже в памяти
+        //Происходит отправка s и B
+        clientB = serverB;
+        serverU = new BigInteger(hash(A.toString(), serverB.toString()));
+        if(serverU.compareTo(BigInteger.ZERO) == 0){
             System.exit(-1);
         }
-        System.out.println("H(A,B):\t");
-        for (byte b : hab) {
-            System.out.print(b + "\s");
-        }
-        System.out.println();
-
-
-        clientX = hash(clientS, clientB.toString());
-        clientS = getClientS(clientB, k, g, clientX, n, clientSmallA, hab);
-        byte[] clientK = hash("", clientS);
-        serverS = getServerS(clientA, serverV, n, serverSmallB, hab);
-        byte[] serverK = hash("", serverS);
-        System.out.println("clientK:\t");
-        for (byte b : clientK) {
-            System.out.print(b + "\s");
-        }
-        System.out.println();
-        System.out.println("----------------------------------------------");
-        System.out.println("serverK:\t");
-        for (byte b : serverK) {
-            System.out.print(b + "\s");
-        }
-        System.out.println();
+        serverS = A.multiply(v.modPow(serverU, N)).modPow(b, N);
+        serverK = new BigInteger(hash(serverS.toString()));
     }
 
-    private static String getServerS(BigInteger a, BigInteger v, BigInteger n, BigInteger b, byte[] u){
-        return a.multiply(v.modPow(new BigInteger(u), n)).modPow(b, n).toString();
+    //Фаза - 2
+    //Клиентская часть
+    private static void getClientMandR() throws NoSuchAlgorithmException {
+        BigInteger M = new BigInteger(hash(
+            new String(xor(hash(N.toString()), hash(g.toString()))),
+            new String(hash(serverI)),
+            clientS.toString() , clientA.toString(), clientB.toString(), k.toString()
+        ));
+        BigInteger R = new BigInteger(hash(clientA.toString(), M.toString(), clientK.toString()));
+        System.out.println("clientR:\t" + R);
     }
 
-    private static String getClientS(BigInteger b, BigInteger k, BigInteger g, byte[] x, BigInteger n, BigInteger a, byte[] u){
-        return b.subtract(k.multiply(g.modPow(new BigInteger(x), n))).modPow(a.add(new BigInteger(u).multiply(new BigInteger(x))), n).toString();
+    //Фаза - 2
+    //Серверная часть
+    private static void getServerMandR() throws NoSuchAlgorithmException {
+        BigInteger M = new BigInteger(hash(
+            new String(xor(hash(N.toString()), hash(g.toString()))),
+            new String(hash(serverI)),
+            serverS.toString() , serverA.toString(), serverB.toString(), k.toString()
+        ));
+        BigInteger R = new BigInteger(hash(serverA.toString(), M.toString(), serverK.toString()));
+        System.out.println("serverR:\t" + R);
     }
 
-    private static BigInteger getB(BigInteger k, BigInteger v, BigInteger g, BigInteger b, BigInteger n){
-        return k.multiply(v).add(g.modPow(b, n)).mod(n);
-    }
 
-    private static BigInteger getA(BigInteger g, BigInteger a, BigInteger n){
-        return g.modPow(a, n);
-    }
-
-    private static BigInteger getV(BigInteger g, byte[] x, BigInteger n){
-        String result = "";
-        for(int i = 0; i < x.length; i++){
-            result = result + g.modPow(new BigInteger(String.valueOf(x[i])), n);
-        }
-        return new BigInteger(result);
-    }
-
-    private static BigInteger getQ(int order, int checkModSimpleMax) {
-        BigInteger checkBigInt = simpleGenerate.getRandomSimpleBigInteger(order, 2000);
+    //Инициализация факторов протокола
+    private static void protocolFactorsInit(int nOrder) {
+        //Получение q
+        BigInteger checkBigInt = simpleGenerate.getRandomSimpleBigInteger(nOrder, 2000);
         while(true){
-            if(simpleGenerate.checkBigIntegerModSimple(checkBigInt.multiply(BigInteger.valueOf(2)).add(BigInteger.ONE), checkModSimpleMax) && 
-            simpleGenerate.checkSimpleMillerRabin(checkBigInt.multiply(BigInteger.valueOf(2)).add(BigInteger.ONE), (int)(Math.log10(order)/Math.log(2)))){
-                return checkBigInt;
+            if(simpleGenerate.checkBigIntegerModSimple(checkBigInt.multiply(BigInteger.valueOf(2)).add(BigInteger.ONE), 2000) && 
+            simpleGenerate.checkSimpleMillerRabin(checkBigInt.multiply(BigInteger.valueOf(2)).add(BigInteger.ONE), (int)(Math.log10(nOrder)/Math.log(2)))){
+                break;
             }
-            checkBigInt = simpleGenerate.getRandomSimpleBigInteger(order, 2000);
+            checkBigInt = simpleGenerate.getRandomSimpleBigInteger(nOrder, 2000);
         }
-    }
-
-    private static BigInteger getN(BigInteger q) {
-        return q.multiply(BigInteger.valueOf(2)).add(BigInteger.ONE);
-    }
-
-    private static BigInteger getG(BigInteger p){
-        BigInteger g = simpleGenerate.getRandomBigInteger(BigInteger.valueOf(2), BigInteger.valueOf(9));
-        while(!g.modPow(p.subtract(BigInteger.ONE).multiply(BigInteger.valueOf(-1)), p).equals(BigInteger.ONE)){
-            g = simpleGenerate.getRandomBigInteger(BigInteger.valueOf(2), BigInteger.valueOf(9));
+        //Получение N
+        N = checkBigInt.multiply(BigInteger.TWO).add(BigInteger.ONE);
+        //Получение g
+        BigInteger phi = N.subtract(BigInteger.ONE);
+        List<BigInteger> pFactor = getFact(phi);
+        for(BigInteger i = BigInteger.TWO; i.compareTo(N) != 1; i = i.add(BigInteger.ONE)){
+            Boolean genOk = true;
+            for(int j = 0; j < pFactor.size(); j++){
+                if(i.modPow(phi.divide(pFactor.get(j)),N).compareTo(BigInteger.ONE) == 0){
+                    genOk = false;
+                    break;
+                }
+            }
+            if(genOk){
+                g = i;
+                break;
+            }
         }
-        return g;
+        //Получение k
+        k = BigInteger.valueOf(3);
     }
 
-    private static BigInteger getK() {
-        return BigInteger.valueOf(3);
+    //Максимум на core i5 2-ого поколения 20 порядок
+    private static List<BigInteger> getFact(BigInteger n){
+        List<BigInteger> pFactor = new ArrayList<BigInteger>();
+        for(BigInteger i = BigInteger.valueOf(2); i.multiply(i).compareTo(n) != 1; i = i.add(BigInteger.ONE)){
+            if(n.mod(i).compareTo(BigInteger.ZERO) == 0){
+                pFactor.add(i);
+                while(n.mod(i).compareTo(BigInteger.ZERO) == 0){
+                    n = n.divide(i);
+                }
+            }
+        }
+        if(n.compareTo(BigInteger.ZERO) == 1){
+            pFactor.add(n);
+        }
+        return pFactor;
     }
 
     private static String getSalt(int order) {
         return simpleGenerate.getRandomBigInteger(order, false).toString();
     }
 
-    private static byte[] hash(String salt, String password) throws NoSuchAlgorithmException {
-        return MessageDigest.getInstance("SHA-512").digest((password + salt).getBytes());
+    private static byte[] hash(String... args) throws NoSuchAlgorithmException {
+        String source = "";
+        for (String string : args) {
+            source = source + string;
+        }
+        return MessageDigest.getInstance("SHA-512").digest((source).getBytes());
     }
+
+    private static byte[] xor(byte[] a, byte[] b) {
+        byte[] result = new byte[Math.min(a.length, b.length)];
+      
+        for (int i = 0; i < result.length; i++) {
+          result[i] = (byte) (((int) a[i]) ^ ((int) b[i]));
+        }
+      
+        return result;
+      }
 }
